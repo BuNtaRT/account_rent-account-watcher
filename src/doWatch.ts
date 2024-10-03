be1e4ae74c333ed5adfd014d6fd011dbc5f7b7d9
BuNtaRT <60608876+BuNtaRT@users.noreply.github.com>
2024-10-03 20:19:07 +0300

    add read steam message

--- src/doWatch.ts
+++ src/doWatch.ts
@@ -1,42 +1,52 @@
 import { AccountActivity } from "./database/account_activity.js";
 import dateFns, { differenceInDays, parse } from "date-fns";
-import { Accounts } from "./database/accounts.js";
+import { Accounts, AccountType } from "./database/accounts.js";
 import axios from "axios";
 import * as cheerio from "cheerio";
 import { InitMail } from "./utils/mailWorking.js";
+import getAuthCode from "./utils/mailModules/getAuthCode.js";
 
 const steamProfile = (id: string) => `https://steamcommunity.com/profiles/${id}`;
 
 export const doWatch = async () => {
 	console.log("doWatch");
-	InitMail();
-	// try {
-	// 	const currentDate = new Date();
-	// 	const now = dateFns.format(currentDate, "yyyy-MM-dd");
-	//
-	// 	const accountActivity = new AccountActivity();
-	// 	const accounts = new Accounts();
-	// 	const activityResult = await accountActivity.getAll(` where date_expired >= '${now}' AND user_count < 3`);
-	//
-	// 	for (const activity of activityResult) {
-	// 		const account = await accounts.getBy(activity.account_id);
-	// 		if (account) {
-	// 			const profile = await axios.get(steamProfile(account.steam_id));
-	// 			const $ = cheerio.load(profile.data);
-	// 			const recentGames = $(".game_info_details").first().html() ?? "";
-	// 			if (!recentGames) return;
-	//
-	// 			const lastPlay = recentGames.replace("\t", "").split(" ");
-	// 			const lastPlayDateString = `${lastPlay.at(-2)} ${lastPlay.at(-1)}`;
-	// 			const lastPlayFormattedString = parse(lastPlayDateString, "d MMM", new Date());
-	// 			const differenceDays = differenceInDays(currentDate, lastPlayFormattedString);
-	//
-	// 			if (differenceDays > 7) {
-	// 				console.log("account - ", account.login, " - ", account.password, " can sell");
-	// 			}
-	// 		}
-	// 	}
-	// } catch (err) {
-	// 	console.log(err);
-	// }
+	const code = await InitMail(getAuthCode);
+	console.log("code", code);
+	//const nonActive = await getNonActive() ?? []
+	//nonActive.forEach((x) => console.log("account - ", x.login, " - ", x.password, " can sell"))
 };
+
+async function getNonActive() {
+	const ids: AccountType[] = [];
+	try {
+		const currentDate = new Date();
+		const now = dateFns.format(currentDate, "yyyy-MM-dd");
+
+		const accountActivity = new AccountActivity();
+		const accounts = new Accounts();
+		const activityResult = await accountActivity.getAll(` where date_expired >= '${now}' AND user_count < 3`);
+
+		for (const activity of activityResult) {
+			const account = await accounts.getBy(activity.account_id);
+			if (account) {
+				const profile = await axios.get(steamProfile(account.steam_id));
+				const $ = cheerio.load(profile.data);
+				const recentGames = $(".game_info_details").first().html() ?? "";
+				if (!recentGames) return;
+
+				const lastPlay = recentGames.replace("\t", "").split(" ");
+				const lastPlayDateString = `${lastPlay.at(-2)} ${lastPlay.at(-1)}`;
+				const lastPlayFormattedString = parse(lastPlayDateString, "d MMM", new Date());
+				const differenceDays = differenceInDays(currentDate, lastPlayFormattedString);
+
+				if (differenceDays > 7) {
+					ids.push(account);
+				} else console.log("cant sell");
+			}
+		}
+	} catch (err) {
+		console.log(err);
+	}
+
+	return ids;
+}
--- src/utils/mailWorking.ts
+++ src/utils/mailWorking.ts
@@ -1,78 +1,78 @@
-import Connection, { ImapMessage } from "imap";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
+
+import Connection, { ImapMessage } from "imap";
 const Imap = require("Imap");
-import * as cheerio from "cheerio";
-import quotedPrintable from "quoted-printable";
-const utf8 = require("utf8");
+import getAuthCode from "./mailModules/getAuthCode.js";
+import { delay } from "./delay.js";
+import log from "./log/log.js";
 
-export const InitMail = () => {
-	console.log("initMail");
-	const imap = new Imap({
-		host: "imap.yandex.ru",
-		port: 993,
-		tls: true,
-	});
+export const InitMail = async (module: MailModuleType): Promise<string> =>
+	new Promise(async (resolve, reject) => {
+		await delay(1000 * 15);
+		const user = "gamecoffee89";
+		const password = "888888";
 
-	imap.once("ready", ready);
-
-	function ready() {
-		imap.openBox("INBOX", true, inbox);
-	}
-
-	function inbox(err: Error, box: Connection.Box) {
-		console.log("inbox");
-		if (err) throw err;
-		const fetchInbox = imap.seq.fetch(box.messages.total + ":*", { bodies: ["HEADER.FIELDS (FROM)", "TEXT"], struct: true });
-
-		fetchInbox.on("message", (message: ImapMessage, seqno: number) => {
-			console.log("Message #%d", seqno);
-			console.log("Message ", message);
-
-			const prefix = "(#" + seqno + ") ";
-
-			message.on("body", (stream, info) => {
-				let buffer = "",
-					count = 0;
-
-				stream.on("data", function (chunk) {
-					if (info.which === "TEXT") {
-						count += chunk.length;
-						buffer += chunk.toString();
-					}
-				});
-				stream.once("end", function () {
-					if (info.which === "TEXT") {
-						const decodeMessage = utf8.decode(quotedPrintable.decode(buffer));
-						const codeBlock = decodeMessage.split("<!-- Auth Code -->")[1].split("<!-- END Auth Code -->")[0];
-						const $ = cheerio.load(codeBlock);
-						const code = $("td")
-							.last()
-							.html()
-							?.replace(/[ \t\n\r]/g, "");
-
-						console.log(`-${code}-`);
-					}
-				});
+		function getLastMail() {
+			const imap = new Imap({
+				user: user,
+				password: password,
+				host: "imap.yandex.ru",
+				port: 993,
+				tls: true,
 			});
 
-			message.once("end", () => {
-				console.log(prefix + "Finished");
-			});
-		});
+			function openInbox(err: Error, box: Connection.Box) {
+				if (err) reject(err.message);
+				const fetchInbox = imap.seq.fetch(box.messages.total + ":*", { bodies: ["TEXT"], struct: true });
 
-		fetchInbox.once("end", function () {
-			imap.end();
-		});
-	}
+				fetchInbox.on("message", (message: ImapMessage) => {
+					message.on("body", (stream, info) => {
+						let buffer = "";
 
-	imap.once("error", function (err: Error) {
-		console.log(err);
+						stream.on("data", function (chunk) {
+							buffer += chunk.toString();
+						});
+						stream.once("end", function () {
+							const code = module(buffer);
+							if (code) resolve(code);
+						});
+					});
+				});
+
+				fetchInbox.once("end", function () {
+					imap.end();
+				});
+			}
+
+			function errorConnection(err: Error) {
+				console.log(err);
+			}
+
+			function endConnection() {
+				console.log("Connection ended");
+			}
+
+			function ready() {
+				imap.openBox("INBOX", true, openInbox);
+			}
+
+			imap.once("ready", ready);
+			imap.once("error", errorConnection);
+			imap.once("end", endConnection);
+
+			imap.connect();
+		}
+
+		const getLastMailInterval = setInterval(getLastMail, 1000 * 30);
+
+		setTimeout(
+			() => {
+				clearInterval(getLastMailInterval);
+				reject("timeout");
+			},
+			1000 * 60 * 5
+		);
 	});
 
-	imap.once("end", function () {
-		console.log("Connection ended");
-	});
-
-	imap.connect();
-};
+type MailModuleType = (val: string) => string | false;
