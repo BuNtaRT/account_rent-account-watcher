9b2e3dbe536c85bd5661c1d612e17908ac3d038e
BuNtaRT <60608876+BuNtaRT@users.noreply.github.com>
2025-03-01 13:13:56 +0300

    db connection edit

--- src/database/db.ts
+++ src/database/db.ts
@@ -6,11 +6,12 @@ dotenv.config();
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
 
 export const pool = new pg.Pool({
 	user: DB_USER,
 	password: DB_PASSWORD,
-	host: "localhost",
+	host: DB_HOST,
 	port: 5432,
 	database: DB_NAME,
 });
